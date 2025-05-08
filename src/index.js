f7faeafb47521cafbc5f24e672c3911c00645353
Yoslen26 <riverson.chico@tecnologicoliceocristiano.edu.ec>
2025-05-07 21:36:27 -0500

    Conexion base de datos Controlador Limpieza de archivos de enrutamiento CRUD

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import routerTodos from './routes/todos.routes.js';
 import { dbClient } from './db.js';
+import * as bodyParser from "express";
+import morgan from 'morgan';
 
 const app = express();
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(routerTodos);
 
